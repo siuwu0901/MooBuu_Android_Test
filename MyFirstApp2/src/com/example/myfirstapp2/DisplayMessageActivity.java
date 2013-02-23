64e27b8cb8d42f997b722ac149ba5ef2ec0cbcf7
AllenWu <siuwu0901@gmail.com>
2013-02-23 19:15:01 +0800

    Allen commit 2/23 7:14

--- MyFirstApp2/src/com/example/myfirstapp2/DisplayMessageActivity.java
+++ MyFirstApp2/src/com/example/myfirstapp2/DisplayMessageActivity.java
@@ -29,7 +29,7 @@ public class DisplayMessageActivity extends Activity {
         //Get Message From Intent
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        message += "hark_0223_5:34";
+        message += "hark_0223_7:14";
         
         //Create Test View
         TextView textView = new TextView(this);
