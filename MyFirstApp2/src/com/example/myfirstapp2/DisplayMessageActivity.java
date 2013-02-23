73b48263a359d2d1920cd8a7039d6ea234a2fcf8
AllenWu <siuwu0901@gmail.com>
2013-02-24 01:48:13 +0800

    Allen Commit Red

--- MyFirstApp2/src/com/example/myfirstapp2/DisplayMessageActivity.java
+++ MyFirstApp2/src/com/example/myfirstapp2/DisplayMessageActivity.java
@@ -30,6 +30,7 @@ public class DisplayMessageActivity extends Activity {
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         message += "red_0224_1:41";
+        message += "red_0224_1:47";
         
         //Create Test View
         TextView textView = new TextView(this);
